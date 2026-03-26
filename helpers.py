b6912c8068e84ae2216d112161a959e77d5752c3
Chdmotriy <Chdmotriy@users.noreply.github.com>
2026-03-26 18:21:11 +0300

    Delete helpers.py

--- helpers.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sqlalchemy import text
-
-def query_df(engine, sql, params=None):
-    with engine.connect() as conn:
-        return pd.read_sql(text(sql), conn, params=params)
